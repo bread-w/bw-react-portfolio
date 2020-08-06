5c73a8d8b57f09d418101840258a8301ae49fd12
Brad Williams <jb.wms89@gmail.com>
2020-08-06 17:00:25 -0400

    creating structure for each page

--- src/components/ContactBody.jsx
+++ src/components/ContactBody.jsx
@@ -1,11 +1,50 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-const ContactBody = () => {
-    return (
-        <div>
-            <h1>Contact Page</h1>
-        </div>
-    );
-};
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+});
 
-export default ContactBody;
+export default function ImgMediaCard() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt=""
+          height="140"
+          image=""
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
