7fef0bc554281af29c1de9bb4d0201e23f9d1e9d
Brad Williams <jb.wms89@gmail.com>
2020-08-13 17:44:50 -0400

    removed publi_url

---
+++ build/precache-manifest.37ac1893bea50d4958b9b929f169e966.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34dd33f2b9cf4f8dbded0f3b57934f67",
+    "url": "http://www.jbwilliams.me/build/index.html"
+  },
+  {
+    "revision": "ac4d739b3466edd2e2e0",
+    "url": "http://www.jbwilliams.me/build/static/css/main.b8942adb.chunk.css"
+  },
+  {
+    "revision": "97d980ded7ab8a4b936b",
+    "url": "http://www.jbwilliams.me/build/static/js/2.cbe4975d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "http://www.jbwilliams.me/build/static/js/2.cbe4975d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac4d739b3466edd2e2e0",
+    "url": "http://www.jbwilliams.me/build/static/js/main.b915be9d.chunk.js"
+  },
+  {
+    "revision": "193f29746adeaa03f477",
+    "url": "http://www.jbwilliams.me/build/static/js/runtime-main.461563c0.js"
+  }
+]);
