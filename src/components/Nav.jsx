8187f525a65105baedf05a67aec1203752b201e0
Brad Williams <jb.wms89@gmail.com>
2020-08-17 17:48:11 -0400

    fixed nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -54,7 +54,7 @@ export default function ButtonAppBar() {
               Brad Williams
             </Typography>
             <Typography variant="h6" className={classes.title}>
-              <Link to="/bw-react-portfolio">
+              <Link to="/">
                 <Button style={navBar}>About</Button>
               </Link>
             </Typography>
